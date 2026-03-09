e177080c33bca8807ec4a094de93f48b21e0f130
OthmanTabeche <otabeche23@gmail.com>
2026-03-09 01:04:38 +0100

    feat: player features
    Add Player sprite and CircleShape base
    
    Introduce CircleShape base class and Player subclass with movement,
    rotation, and triangle drawing. Add player-related constants and set up
    updatable/drawable sprite groups in main; instantiate Player and update
    draw/update logic in the main loop

--- main.py
+++ main.py
@@ -2,22 +2,38 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    dt = 0
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            dt = clock.tick(60) / 1000
+
+        dt = clock.tick(60) / 1000
         screen.fill("black")
+
+        for p in drawable:
+            p.draw(screen)
+
+        updatable.update(dt)
         pygame.display.update()
 
 
